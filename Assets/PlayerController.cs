12652915f74e3dd1045a3ca26d98f9d0f605e98c
Kaustuv Prateeq <kaustuvprateeq200@gmail.com>
2022-01-29 14:44:13 -0800

    KP03 He dashesssss

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    //idk why i introduce variables here
     public Vector2 speed = new Vector2(10, 7);
     Vector3 movement;
     public float xVelocity;
     public float yVelocity;
+
     // Update is called once per frame
     void Update()
     {
+        //WASD keypresses
         xVelocity = Input.GetAxis("Horizontal");
         yVelocity = Input.GetAxis("Vertical");
 
+        //dash go brrrr
+        if (Input.GetKeyDown("left shift"))
+        {
+            speed = new Vector2(20, 14);
+        }
+        else if (Input.GetKeyUp("left shift"))
+        {
+            speed = new Vector2(10, 7);
+        }
 
+        //character flips on left/right movement
         if (xVelocity < 0f)
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
@@ -25,6 +38,7 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(1f, 1f, 1f);
         }
 
+        //move player
         movement = new Vector3(speed.x * xVelocity, speed.y * yVelocity , 0);
         movement *= Time.deltaTime;
         transform.Translate(movement);
