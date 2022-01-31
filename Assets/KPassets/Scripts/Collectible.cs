163242468759115f227a80f3bd16a0455913c5a6
Kaustuv Prateeq <kaustuvprateeq200@gmail.com>
2022-01-30 19:18:03 -0800

    KP03 Picks Up Items

---
+++ Assets/KPassets/Scripts/Collectible.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+
+    public float pickUpDistance = 1.5f;
+    public GameObject player;
+
+    public float distance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            //scan to see the distance
+            distance = Vector3.Distance(transform.position, player.transform.position);
+
+            //if distance < pickUpDistance do something
+            if (distance < pickUpDistance)
+            {
+                //TODO:send the data to inventory
+
+                //destroy the object
+                Destroy(gameObject);
+
+            }
+        }
+    }
+
+}
