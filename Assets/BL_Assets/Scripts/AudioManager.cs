3906f5eb1a56a0dc36366e20ee975f976c9c1650
brulea <bruceeleavitt@gmail.com>
2022-05-25 18:54:30 -0700

    Title Screen aspect ratio fix

--- Assets/BL_Assets/Scripts/AudioManager.cs
+++ Assets/BL_Assets/Scripts/AudioManager.cs
@@ -42,4 +42,16 @@ public class AudioManager : MonoBehaviour
             
         s.audioSource.Play();
     } 
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound " + name + " not found!");
+            return;
+        }
+
+        s.audioSource.Stop();
+    }
 }
