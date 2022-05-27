6cd8e0129aa3d198e5eccbb73995836f98a1f113
brulea <bruceeleavitt@gmail.com>
2022-05-27 01:11:32 -0700

    ITEM DESCRIPTIONS SHOW

--- Assets/KPassets/Scripts/SLOTS.cs
+++ Assets/KPassets/Scripts/SLOTS.cs
@@ -11,15 +11,31 @@ public class SLOTS : MonoBehaviour
     public int ID;
     public string type;
     public string description;
+    public Text dialogtext;
     public bool empty;
     public Sprite icon;
-
+    public GameObject txtbox;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    public void displayDescription()
+    {
+        if (txtbox.activeInHierarchy)
+        {
+            txtbox.SetActive(false);
+            dialogtext.text = description;
+            txtbox.SetActive(true);
+        }
+        else
+        {
+            txtbox.SetActive(true);
+            dialogtext.text = description;
+        }
     }
 
     // Update is called once per frame
@@ -27,4 +43,6 @@ public class SLOTS : MonoBehaviour
     {
         this.GetComponent<Image>().sprite = icon;
     }
+
+
 }
